bf7c66bf0f71eb918ce422f4f11223c3374ea631
03y <98747393+03y@users.noreply.github.com>
2024-10-18 16:01:51 +0000

    some initial progress on backend

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,42 @@
 package main
 
 import (
-    "os"
-    "fmt"
+    "context"
     "net/http"
+    "fmt"
+    "os"
     "github.com/gin-gonic/gin"
+    "github.com/jackc/pgx/v5"
 )
 
 const NAME      string = "🍺 boozer"
 const VERSION   string = "0.1-Alpha"
 
-func uptime(c *gin.Context) {
-    c.IndentedJSON(http.StatusOK, "hello boozers!")
+var   db_connection pgx.Conn
+
+func connect_db() {
+    // urlExample := "postgres://username:password@localhost:5432/database_name"
+    db_connection, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+    if err != nil {
+        fmt.Printf("WARNING: Unable to connect to database: %v\n", err)
+    }
+    fmt.Printf("Successfully connected to database!")
+    defer db_connection.Close(context.Background())
+}
+
+func db_query() {
+    var name string
+    err := db_connection.QueryRow(context.Background(), "SELECT username FROM users WHERE user_id=1;").Scan(&name)
+    if err != nil {
+        fmt.Printf("QueryRow failed: %v\n", err)
+        os.Exit(1)
+    }
+    fmt.Println(name)
+}
+
+func hello(c *gin.Context) {
+    db_query()
+    c.IndentedJSON(http.StatusOK, "hello!")
 }
 
 func main() {
@@ -22,8 +47,11 @@ func main() {
         return
     }
 
+    connect_db()
+
+    // Setup API routes
     router := gin.Default()
-    router.GET("/hello", uptime)
+    router.GET("/hello", hello)
 
     var listen string = os.Args[1]
     fmt.Printf("\nLets get boozing! 🍻\nListening on %s...\n\n", listen)
