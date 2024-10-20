23ba589abed2eb85519e3c26c3306a5c4cfdaf50
03y <98747393+03y@users.noreply.github.com>
2024-10-20 17:04:53 +0000

    update db test data script

--- db/test_data.sql
+++ db/test_data.sql
@@ -1,3 +1,22 @@
-INSERT INTO users (username, password, reg_date) VALUES ('Pibble', 'lemur', now());
-INSERT INTO items (name, abv, added) VALUES ('Tennents', 4.0, now());
-INSERT INTO consumptions (item_id, user_id, time) VALUES (1, 1, now());A
+-- create users
+INSERT INTO users (username, password, created) VALUES ('Pibble', 'lemur', EXTRACT(epoch FROM now()));
+INSERT INTO users (username, password, created) VALUES ('Glorp', 'lemur', EXTRACT(epoch FROM now()));
+INSERT INTO users (username, password, created) VALUES ('Gleeb', 'lemur', EXTRACT(epoch FROM now()));
+INSERT INTO users (username, password, created) VALUES ('Gnarp', 'lemur', EXTRACT(epoch FROM now()));
+
+-- add some items
+INSERT INTO items (name, units, added) VALUES ('Tennents', 2.272, EXTRACT(epoch FROM now()));
+INSERT INTO items (name, units, added) VALUES ('Staropramen', 3.3, EXTRACT(epoch FROM now()));
+INSERT INTO items (name, units, added) VALUES ('Guinness', 2.3, EXTRACT(epoch FROM now()));
+
+-- add some consumptions
+-- todo: dont hardcode the user and item ids here
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (1, 1, now());
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (1, 2, now());
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (1, 2, now());
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (2, 3, now());
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (2, 3, now());
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (2, 3, now());
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (3, 1, now());
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (3, 3, now());
+INSERT INTO consumptions (consumption_id, user_id, item_id, time) VALUES (4, 2, now());
