cf195a45e5535ae6cc605e9cebd7891544ded268
03y <98747393+03y@users.noreply.github.com>
2025-08-30 10:29:40 +0100

    add item tests

--- backend/tests/run_tests.sh
+++ backend/tests/run_tests.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# User tests
+username=$(openssl rand -hex 8)
+password=$(openssl rand -hex 8)
 
-# generate random string as username
-random_string=$(openssl rand -hex 8)
-hurl -v --test --variable random_string=$random_string users.hurl
+echo "Running user tests..."
+hurl --test \
+    --variable username=$username \
+    --variable password=$password \
+    users.hurl
+
+item_name=$(openssl rand -hex 8)
+item_units=2.2
+
+echo "Running item tests..."
+hurl --test \
+    --variable username=$username \
+    --variable password=$password \
+    --variable item_name=$item_name \
+    --variable item_units=$item_units \
+    items.hurl
 
